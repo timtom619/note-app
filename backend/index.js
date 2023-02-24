ff0d241f689b57528730b8f02f465064cbd9bebd
Timothy Tomas <timothyjtee@gmail.com>
2023-02-23 20:46:03 -0800

    add new files

--- backend/index.js
+++ backend/index.js
@@ -1,53 +1,31 @@
 const express = require('express')
-// const morgan = require('morgan')
 const app = express()
-const cors = require('cors')
-
-// morgan.token('data', (request, response) => {
-//   return JSON.stringify(request.body)
-// })
-// const requestLogger = morgan(':method :url :status :res[content-length] - :response-time ms :data') 
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.use(cors())
-app.use(express.json())
-app.use(requestLogger)
-app.use(express.static('build'))
 
 let notes = [
   {
     id: 1,
     content: "HTML is easy",
+    date: "2022-01-10T17:30:31.098Z",
     important: true
   },
   {
     id: 2,
-    content: "Browser can execute only JavaScript",
+    content: "Browser can execute only Javascript",
+    date: "2022-01-10T18:39:34.091Z",
     important: false
   },
   {
     id: 3,
     content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2022-01-10T19:20:14.298Z",
     important: true
   }
 ]
 
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
-})
-
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
 })
 
 const generateId = () => {
@@ -78,17 +56,8 @@ app.post('/api/notes', (request, response) => {
   response.json(note)
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
-
-  response.json(note)
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -98,9 +67,18 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-app.use(unknownEndpoint)
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
 
-const PORT = process.env.PORT || 3001
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
